c23e5e6396aa827b53f81d8382cd22ac3e1fc254
Tyler Richman <Tyler.Richman@erg.com>
2024-07-25 09:09:55 -0400

    Update CreateConcaveHull.R
    Make the concavity setting a editable parameter.

--- CreateConcaveHull.R
+++ CreateConcaveHull.R
@@ -3,16 +3,17 @@ rm(list=ls())
 library(sf)
 library(concaveman)
 
-##### Only Edit PWS_ID #####
+##### Set the Parameters Below #####
 
 PWS_ID = "OH5300012"
+concavity_setting = 1
 
-############################
+####################################
 
 fc <- st_read(dsn = paste0(PWS_ID, ".gdb"), layer = "_03_digitized_features_vertices")
 
 coordinates <- st_coordinates(fc)
-hull <- concaveman(coordinates, concavity = 2)
+hull <- concaveman(coordinates, concavity = concavity_setting)
 
 hull_sf <- st_sf(geometry = st_sfc(st_polygon(list(hull))), crs = st_crs(fc))
 
