6a6c71cbc4ae713b208a73740651d4a27c3f60df
Tyler Richman <Tyler.Richman@erg.com>
2024-08-05 17:12:59 -0400

    Update CreateConcaveHull.R

--- CreateConcaveHull.R
+++ CreateConcaveHull.R
@@ -5,21 +5,23 @@ library(concaveman)
 
 ##### Set the Parameters Below #####
 
-PWS_ID = "OH5300012"
-concavity_setting = 1
+PWS_ID = "OH8301512"
+concavity_setting = 2
+
+workspace_dir = "C:/Users/TRichman.ERG/Tyler/Projects/2024/PWS Georeferencing/GIS/Tool Runs"
 
 ####################################
 
-fc <- st_read(dsn = paste0(PWS_ID, ".gdb"), layer = "_03_digitized_features_vertices")
+fc <- st_read(dsn = file.path(workspace_dir, paste0(PWS_ID, ".gdb")), layer = "_03_digitized_features_vertices")
 
 coordinates <- st_coordinates(fc)
 hull <- concaveman(coordinates, concavity = concavity_setting)
 
 hull_sf <- st_sf(geometry = st_sfc(st_polygon(list(hull))), crs = st_crs(fc))
 
-if (!dir.exists(paste0("./", PWS_ID))){
-  dir.create(paste0("./", PWS_ID))  
+if (!dir.exists(file.path(workspace_dir, paste0("./", PWS_ID)))){
+  dir.create(file.path(workspace_dir, paste0("./", PWS_ID)))
 }
 
-st_write(hull_sf, paste0("./", PWS_ID, "/", PWS_ID, ".shp"))
+st_write(hull_sf, file.path(workspace_dir, paste0("./", PWS_ID, "/", PWS_ID, ".shp")))
 
